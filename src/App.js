951913786429656d8e420d6a277e40c05796d97e
Natalie Chin <chinnh@mcmaster.ca>
2017-10-20 22:44:26 -0400

    contains starter updated readme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css'; 
+import './App.css';  
 import * as Web3 from 'web3'; 
-import * as BigNumber from 'bignumber.js'; 
 
-class App extends Component { 
-  async componentDidMount() {  
-     const provider = new Web3.providers.HttpProvider('http://localhost:8545'); 	
-  } 
+class App extends Component {
+  async componentDidMount () { 
+
+
+            const provider = new Web3.providers.HttpProvider('http://localhost:8545'); 
+  }
   render() {
     return (
       <div className="App">
@@ -15,9 +16,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
